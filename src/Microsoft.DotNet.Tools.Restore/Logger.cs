df2f696b9baaeb0f8c12751cf7af706b88ba108f
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-10-15 15:59:06 -0700

    maybe we can have cross-plat restore?

---
+++ src/Microsoft.DotNet.Tools.Restore/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NuGet.Logging;
+
+namespace Microsoft.DotNet.Tools.Restore
+{
+    internal class Logger : ILogger
+    {
+        public void LogDebug(string data)
+        {
+            Console.WriteLine($"debug: {data}");
+        }
+
+        public void LogError(string data)
+        {
+            Console.WriteLine($"error: {data}");
+        }
+
+        public void LogInformation(string data)
+        {
+            Console.WriteLine($"info : {data}");
+        }
+
+        public void LogVerbose(string data)
+        {
+            Console.WriteLine($"trace: {data}");
+        }
+
+        public void LogWarning(string data)
+        {
+            Console.WriteLine($"warn : {data}");
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.Tools.Restore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using NuGet.Commands;
+using NuGet.Configuration;
+using NuGet.ProjectModel;
+
+namespace Microsoft.DotNet.Tools.Restore
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var cmd = new RestoreCommand(
+                new Logger(),
+                new RestoreRequest(
+                    JsonPackageSpecReader.GetPackageSpec(
+                        File.OpenRead(Path.Combine(Directory.GetCurrentDirectory(), "project.json")),
+                        Path.GetFileName(Directory.GetCurrentDirectory()),
+                        Path.Combine(Directory.GetCurrentDirectory(), "project.json")),
+                    new[] {
+                        new PackageSource("https://www.myget.org/F/aspnetcidev/api/v3/index.json", "AspNetCIDev"),
+                        new PackageSource("https://www.myget.org/F/dotnet-core/api/v3/index.json", "dotnet-core"),
+                        new PackageSource("https://api.nuget.org/v3/index.json", "api.nuget.org"),
+                    },
+                    @"C:\Users\anurse\.dnx\packages"));
+            var result = cmd.ExecuteAsync().Result;
+        }
+    }
+}
