f5dc6dc5fe31cfae9a1ef8ce4e5322312469f34c
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-02-02 10:04:50 -0800

    wip

---
+++ scripts/dotnet-cli-build/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.DotNet.Cli.Build.Framework;
+
+namespace Microsoft.DotNet.Cli.Build
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var buildModel = BuildModel.Create<Program>();
+        
+            Reporter.Output.WriteBanner("Starting .NET Core CLI build");
+        }
+    }
+}
